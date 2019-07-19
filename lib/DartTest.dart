670517a8d5928af617ad0ad41938c2fa3143c6e2
mac <1010479088@qq.com>
2019-07-19 13:20:26 +0800

    1.小测试

---
+++ lib/DartTest.dart
@@ -0,0 +1,20 @@
+
+class DartTest {
+  int x, y;
+  num x1, y1;
+  String x2, y2;
+
+  num get x3 => x + y;
+
+  set x3(int x) => x = x - y;
+
+  getDataFromNet() async {
+
+  }
+
+  getDaa() {
+    return x;
+  }
+}
+
+enum Sex { MALE, FEMALE }
